aa3a71dd40403c667301861880f73933eda65f99
안세용 <seyong@anseyong-ui-MacBookPro.local>
2023-05-20 18:02:52 +0900

    msg

--- frontend/src/components/MemoryForm.jsx
+++ frontend/src/components/MemoryForm.jsx
@@ -1,6 +1,7 @@
 import '../style/memoryform.css'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
+import { Link } from 'react-router-dom'
 import axios from 'axios'
 export function MemoryForm() {
     const navigate = useNavigate()
@@ -20,10 +21,6 @@ export function MemoryForm() {
 
     const onClick = () => {
         client.post('http://127.0.0.1:8000/postMessage',{message})
-        .then(res => {
-            navigate('/menu')
-        })
-        
     }
 
     return(
@@ -31,7 +28,9 @@ export function MemoryForm() {
             <form id='memoryform'>
                 <input placeholder="이름을 입력해 주세요 (최대 14글자)" id='memoryformname' name='author' onChange={onChange} maxLength='14'/>
                 <textarea placeholder="한줄메세지를 입력해 주세요 (최대 30글자)" id='memoryformtext' name='text' onChange={onChange} maxLength='30'/>
-                <button id='memoryformsubmit' onClick={onClick}>Submit</button>
+                <Link to='/menu'>
+                    <button id='memoryformsubmit' onClick={onClick}>Submit</button>
+                </Link>
             </form>
         </div>
     )

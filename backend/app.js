991285c7c07d6706a62225ec11080fbaad251cb0
안세용 <seyong@anseyong-ui-MacBookPro.local>
2023-05-20 12:44:46 +0900

    msg

--- backend/app.js
+++ backend/app.js
@@ -8,13 +8,13 @@ const logger = require('./winston');
 //const sp = new SerialPort("/dev/ttyAMA0", { baudRate:9600, autoOpen:false });
 
 app.use(morgan("combined", {stream : logger.stream}));
-/*
+
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
   password : '1029',
   database : 'mediaart'
-});*/
+});
 
 app.use(express.json())
 app.use(function (error, req, res, next) {
@@ -28,24 +28,23 @@ app.use(function (error, req, res, next) {
 
 var cors = require('cors');
 app.use(cors());
-//connection.connect();
+connection.connect();
 
 
 app.get('/getTree',(req, res)=>{
   let recUserinfo = req.body.account
   logger.info(`getTree`);
-  const msg = [
+  /*const msg = [
     {
         'author' : 'seyong',
         'text' : 'msg'
     }
-  ]
-  res.send(msg)
-  /*connection.query(`SELECT * FROM message`, (error, rows) => {
+  ]*/
+  connection.query(`SELECT * FROM message`, (error, rows) => {
     if (error) throw error;
     res.send(rows)
     logger.info(`getTree: ${rows}`);
-  });*/
+  });
 });
 
 
@@ -53,7 +52,7 @@ app.post('/postMessage', (req,res) => {
     const recAuthor = req.body.author
     const recMessage = req.boey.message
     logger.info(`postMessage: ${recAuthor}, ${recMessage}`);
-    /*connection.query(`INSERT INTO message VALUES ('${recAuthor}', '${recMessage}')`, (error) => {
+    connection.query(`INSERT INTO message VALUES ('${recAuthor}', '${recMessage}')`, (error) => {
         if (error) throw error;
         }
       )
@@ -68,7 +67,7 @@ app.post('/postMessage', (req,res) => {
                 console.log("메세지가 정상적으로 입력되었습니다.");
             }
         });
-    });*/
+    });
     }
 )
 
